1f0a3b841101b721413efffb4ea4e2e5f7e5ad46
LENA EIVY <applena@gmail.com>
2018-09-24 14:22:15 -0700

    product instancces entered

--- js/app.js
+++ js/app.js
@@ -8,6 +8,28 @@ function ProductInstances(productName, filepath) {
   this.numberOfTimesClicked = [];
 }
 
+// render the object instances
+new ProductInstances("R2D2 Bag", "img/bag.jpg");
+new ProductInstances("Banana Slicer", "img/banana.jpg");
+new ProductInstances("Bathroom Tablet Holder", "img/bathroom.jpg");
+new ProductInstances("Open Toed Rainboots", "img/boots.jpg");
+new ProductInstances("All In One Breakfast Maker", "img/breakfast.jpg");
+new ProductInstances("Meatball Flavored Bubblegum", "img/bubblegum.jpg");
+new ProductInstances("Puffy Chair", "img/chair.jpg");
+new ProductInstances("Cthulhu Action Figure", "img/cthulhu.jpg");
+new ProductInstances("Duck Beak For Your Dog", "img/dog-duck.jpg");
+new ProductInstances("Dragon Meat", "img/dragon.jpg");
+new ProductInstances("Pen Utensils", "img/pen.jpg");
+new ProductInstances("Pet Friendly Sweeper", "img/pet-sweep.jpg");
+new ProductInstances("Pizza Scissors", "img/scissors.jpg");
+new ProductInstances("Shark Sleeping Bag", "img/shark.jpg");
+new ProductInstances("Baby Sweaper", "img/sweep.jpg");
+new ProductInstances("Tauntaun Sleeping Bag", "img/tauntaun.jpg");
+new ProductInstances("Unicorn Meat", "img/unicorn.jpg");
+new ProductInstances("Tenticle USB", "img/usb.gif");
+new ProductInstances("Self Watering Water Can", "img/water-can.jpg");
+new ProductInstances("Impossible Wine Glass", "img/wine-glass.jpg");
+
 //event listener
   //listening for click
 
@@ -26,8 +48,6 @@ function ProductInstances(productName, filepath) {
       //put image name in alt
 
     //tracks which image got the click -- push index number to an array
-
-// render the object instances
 
 // at end, run a for loop through the array that displayed the images and add the index numbers together
 
