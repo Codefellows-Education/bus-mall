3e097c3867fa078027a05bdc13dfc409473ef6ee
LENA EIVY <applena@gmail.com>
2018-09-25 12:44:28 -0700

    updated styping to have header

--- js/app.js
+++ js/app.js
@@ -6,43 +6,61 @@ var imageTwo = document.getElementById('image-two');
 var imageThree = document.getElementById('image-three');
 var allProducts = [];
 var productsShown = [];
+var maxClickedAllowed = 25;
 
 //constructor function
-function ProductInstances(productName, filepath) {
+function ProductInstances(productName, filepath, numberOfTimesShown=0, numberOfTimesClicked=0) {
   this.productName = productName;
   this.filepath = filepath;
-  this.numberOfTimesShown = 0;
-  this.numberOfTimesClicked = 0;
+  this.numberOfTimesShown = numberOfTimesShown;
+  this.numberOfTimesClicked = numberOfTimesClicked;
 
   allProducts.push(this);
 }
 
 // render the object instances
-new ProductInstances("R2D2 Bag", "img/bag.jpg");
-new ProductInstances("Banana Slicer", "img/banana.jpg");
-new ProductInstances("Bathroom Tablet Holder", "img/bathroom.jpg");
-new ProductInstances("Open Toed Rainboots", "img/boots.jpg");
-new ProductInstances("All In One Breakfast Maker", "img/breakfast.jpg");
-new ProductInstances("Meatball Flavored Bubblegum", "img/bubblegum.jpg");
-new ProductInstances("Puffy Chair", "img/chair.jpg");
-new ProductInstances("Cthulhu Action Figure", "img/cthulhu.jpg");
-new ProductInstances("Duck Beak For Your Dog", "img/dog-duck.jpg");
-new ProductInstances("Dragon Meat", "img/dragon.jpg");
-new ProductInstances("Pen Utensils", "img/pen.jpg");
-new ProductInstances("Pet Friendly Sweeper", "img/pet-sweep.jpg");
-new ProductInstances("Pizza Scissors", "img/scissors.jpg");
-new ProductInstances("Shark Sleeping Bag", "img/shark.jpg");
-new ProductInstances("Baby Sweeper", "img/sweep.png");
-new ProductInstances("Tauntaun Sleeping Bag", "img/tauntaun.jpg");
-new ProductInstances("Unicorn Meat", "img/unicorn.jpg");
-new ProductInstances("Tenticle USB", "img/usb.gif");
-new ProductInstances("Self Watering Water Can", "img/water-can.jpg");
-new ProductInstances("Impossible Wine Glass", "img/wine-glass.jpg");
+function initalizeDatabase() {
+  new ProductInstances("R2D2 Bag", "img/bag.jpg");
+  new ProductInstances("Banana Slicer", "img/banana.jpg");
+  new ProductInstances("Bathroom Tablet Holder", "img/bathroom.jpg");
+  new ProductInstances("Open Toed Rainboots", "img/boots.jpg");
+  new ProductInstances("All In One Breakfast Maker", "img/breakfast.jpg");
+  new ProductInstances("Meatball Flavored Bubblegum", "img/bubblegum.jpg");
+  new ProductInstances("Puffy Chair", "img/chair.jpg");
+  new ProductInstances("Cthulhu Action Figure", "img/cthulhu.jpg");
+  new ProductInstances("Duck Beak For Your Dog", "img/dog-duck.jpg");
+  new ProductInstances("Dragon Meat", "img/dragon.jpg");
+  new ProductInstances("Pen Utensils", "img/pen.jpg");
+  new ProductInstances("Pet Friendly Sweeper", "img/pet-sweep.jpg");
+  new ProductInstances("Pizza Scissors", "img/scissors.jpg");
+  new ProductInstances("Shark Sleeping Bag", "img/shark.jpg");
+  new ProductInstances("Baby Sweeper", "img/sweep.png");
+  new ProductInstances("Tauntaun Sleeping Bag", "img/tauntaun.jpg");
+  new ProductInstances("Unicorn Meat", "img/unicorn.jpg");
+  new ProductInstances("Tenticle USB", "img/usb.gif");
+  new ProductInstances("Self Watering Water Can", "img/water-can.jpg");
+  new ProductInstances("Impossible Wine Glass", "img/wine-glass.jpg");
+}
+
+//event listeners
+function setUpEventListener () {
+  imageOne.addEventListener('click', randomImageGenerator);
+  imageTwo.addEventListener('click', randomImageGenerator);
+  imageThree.addEventListener('click', randomImageGenerator);
+}
+
+// function removeEventListeners () {
+//   imageOne.removeEventListeners('click');
+//   imageTwo.removeEventListeners('click');
+//   imageThree.removeEventListeners('click');
+// }
 
 function randomImageGenerator () {
 
   //generaes the final account form
-  if (productsShown.length >= 75) {
+  if (maxClickedAllowed === 0) {
+    //removeEventListeners();
+
     var resultsSection = document.getElementById('results-hidden');
     resultsSection.id = 'results-show';
     imageOne.className = 'hidden';
@@ -66,6 +84,7 @@ function randomImageGenerator () {
     for (var j = 0; j < allProducts.length; j++){
       if (event.target.alt === allProducts[j].productName) {
         allProducts[j].numberOfTimesClicked++;
+        maxClickedAllowed--;
         console.log(allProducts[j].numberOfTimesClicked);
       }
     }
@@ -144,8 +163,8 @@ if (productsShown.length > 75) {
   }
 }
 
-imageOne.addEventListener('click', randomImageGenerator);
-imageTwo.addEventListener('click', randomImageGenerator);
-imageThree.addEventListener('click', randomImageGenerator);
 
+initalizeDatabase();
+setUpEventListener();
 randomImageGenerator();
+
